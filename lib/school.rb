26c5d38f34d2ab332a2f2f24fd666103865e19f3
Tom White <eugene.white@gmail.com>
2019-05-30 15:40:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  attr_accessor :name
+  
+  ROSTER = {}
+  
+  def initialize(school_name)
+    @name = school_name
+  end
+  
+  def add_student (student_name, grade)
+    ROSTER[grade] << student_name
+  end
+  
+end
