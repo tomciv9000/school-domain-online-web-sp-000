da0434102751224c7c36d8d5f96cff091d7a3ad5
Tom White <eugene.white@gmail.com>
2019-05-30 15:54:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,9 +7,11 @@ class School
   end
   
   def add_student(student_name, grade)
-    @roster.keys
-    @roster[grade] = []
-    @roster[grade] << student_name
+    if @roster.keys.include?(grade)
+      @roster[grade] << student_name
+    else @roster[grade] = []
+      @roster[grade] << student_name
+    end
   end
   
 end
